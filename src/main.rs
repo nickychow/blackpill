2175761bc57f5d2867ad47fcdcb888685777b7f6
Nick Chow <nchow@raywhite.com>
2023-04-19 23:35:27 +1000

    bump dependencies

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,8 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use stm32f4xx_hal as hal;
-
-use crate::hal::{pac, prelude::*};
+use fugit::{Duration, ExtU32};
+use hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -22,6 +21,8 @@ fn main() -> ! {
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
+        let gpioa = dp.GPIOA.split();
+        let button = gpioa.pa0;
         // Set up the LED. On the Mini-F4 it's connected to pin PC13.
         let gpioc = dp.GPIOC.split();
         let mut led = gpioc.pc13.into_push_pull_output();
@@ -33,6 +34,12 @@ fn main() -> ! {
         // Create a delay abstraction based on general-pupose 32-bit timer TIM5
         let mut delay = dp.TIM5.delay_us(&clocks);
 
+        let mut counter = dp.TIM1.counter_ms(&clocks);
+
+        let mut del_var: Duration<u32, 1, 1000> = 2001.millis();
+
+        led.set_low();
+
         loop {
             // // On for 1s, off for 3s.
             led.set_high();
@@ -42,7 +49,7 @@ fn main() -> ! {
             // // or use `fugit::ExtU32` trait
             // delay.delay(1.secs());
             // led.toggle();
-            delay.delay(5.secs());
+            delay.delay(2.secs());
         }
     }
     loop {
